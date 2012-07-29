bbbeec5eaf19321de8cd11ecb26fcfc08d45e73a
David Davis <david@wippoc.com>
2012-07-28 23:04:15 -0700

    db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120729051316) do
+ActiveRecord::Schema.define(:version => 20120729051623) do
 
   create_table "departments", :force => true do |t|
     t.string   "name"
