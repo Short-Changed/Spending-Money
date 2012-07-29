08003501e7aa643560f84846d3c535b91fc5f3a2
Phill Kenoyer <phill@ors.bz>
2012-07-29 15:02:56 -0700

    calc budget and re-show values

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,7 @@ class WelcomeController < ApplicationController
   expose(:yearly_revenues) { location.revenues.order('year asc') }
   expose(:revenues)       { yearly_revenues.collect{|r| [r.year.to_s, r.amount]}.to_json }
   expose(:revenue)        { yearly_revenues.last.amount }
-  expose(:your_chunk)     { revenue / population }
+  expose(:your_chunk)     { (revenue / population) - User.find(1).expenditures.sum(:amount) }
 
   def index
   end
