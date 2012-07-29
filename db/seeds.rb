43bbdcf099b11fc108cf3713a383409d74e23d30
David Davis <david@wippoc.com>
2012-07-28 23:04:03 -0700

    db/seeds -- dummy data for app initialization

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+function = Function.create!(name: "Health")
+
+dept = function.departments.create!(name: "Water Department")
+
+dept.programs.create!(name: "Water Treatment")
