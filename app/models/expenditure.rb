1d4d8487633883db70de50d729e3611b574959c7
David Davis <david@wippoc.com>
2012-07-29 10:10:04 -0700

    annotate --exclude tests,fixtures

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: expenditures
+#
+#  id              :integer          not null, primary key
+#  year            :integer
+#  amount          :integer
+#  budgetable_type :string(255)
+#  budgetable_id   :integer
+#  user_id         :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class Expenditure < ActiveRecord::Base
   belongs_to :budgetable, :polymorphic => true
   # attr_accessible :title, :body
