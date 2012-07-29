968a108dc003456bcafef614e5eda3f128e3ab8e
David Davis <david@wippoc.com>
2012-07-28 19:39:24 -0700

    run: annotate --exclude tests,fixtures

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: programs
+#
+#  id             :integer          not null, primary key
+#  parent_id      :integer
+#  reference_code :string(255)
+#  name           :string(255)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Program < ActiveRecord::Base
   # attr_accessible :title, :body
 end
