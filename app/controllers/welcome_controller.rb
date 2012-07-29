979c3bfcf12fa7ce7a16fcf94880bf1acca9bc2c
Phill Kenoyer <phill@ors.bz>
2012-07-29 11:30:05 -0700

    Fixed charts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
   expose(:location)       { Location.find_by_name(location_name) }
   expose(:population)     { location.population }
   expose(:yearly_revenues) { location.revenues.order('year asc') }
-  expose(:revenues)       { yearly_revenues.collect(&:amount).to_json }
+  expose(:revenues)       { yearly_revenues.collect{|r| [r.year.to_s, r.amount]}.to_json }
   expose(:revenue)        { yearly_revenues.last.amount }
   expose(:your_chunk)     { revenue / population }
 
