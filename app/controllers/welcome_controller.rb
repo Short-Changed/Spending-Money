453f36fa82bc39a32d05ab913b80327a12faed12
Phill Kenoyer <phill@ors.bz>
2012-07-29 01:41:22 -0700

    Added the home page graph

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @current_revenue = Location.find_by_name('City of San Francisco').revenues.order('year asc').last.amount
+    @current_population = Location.find_by_name('City of San Francisco').population
+    @your_chunk = @current_revenue / @current_population
+    @revenues = Location.find_by_name('City of San Francisco').revenues.order('year desc').collect { |r| r.amount }.to_json
   end
 end
